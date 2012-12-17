72d02c855684cbc84f756f69112c55b69c13eb0a
Uri Barenholz <uri.barenholz@gmail.com>
2012-12-17 17:29:25 +0200

    some more initial work

--- BioLab/Analysis/GrowthRate.hs
+++ BioLab/Analysis/GrowthRate.hs
@@ -6,6 +6,7 @@ module BioLab.Analysis.Biomass (
 where
 import Statistics.Sample (Sample)
 import Data.Vector.Unboxed as U
+import Data.Maybe (fromMaybe)
 
 newtype RawMeasurement = RawMeasurement {mVal :: Double}
 newtype NormalizedMeasurement = NormalizedMeasurement {nmVal :: Double}
@@ -18,6 +19,12 @@ normalize (Background bg) (DetectionThreshold dt) (RawMeasurement rm)= Normalize
 
 minDoublingTime :: U.Vector (UTCTime,NormalizedMeasurement) -> NominalDiffTime
 
-doublingTime :: U.Vector (UTCTime, NormalizedMeasurement) -> U.Vector (UTCTime,NominalDiffTime)
+doublingTime (Integral a) :: Maybe a -> U.Vector (UTCTime, NormalizedMeasurement) -> U.Vector (UTCTime,NominalDiffTime)
+doublingTime mws mes = map (calcDoublingTime . U.take $ window_size) . takeWhile (U.length >= window_size) . U.tails . U.map (\(x,y) -> (fromIntegral . toSeconds $ x, logBase 10 . nmVal $ y)) $ mes
+    where
+        window_size = fromMaybe windowSize $ mws
 
-
+calcDoublingTime :: U.Vector (Double,Double) -> NominalDiffTime
+calcDoublingTime xys = fromSeconds . (1/) . snd . robustFit xs $ ys
+    where
+        (xs,ys) = U.unzip xys

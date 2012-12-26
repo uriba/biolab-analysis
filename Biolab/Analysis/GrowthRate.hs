7100d950a83012ef5684c9683d49f41241194e5f
Uri Barenholz <uri.barenholz@gmail.com>
2012-12-26 12:43:04 +0200

    doubling time can now gracefully fail via Nothing. Negative values are discarded

--- Biolab/Analysis/GrowthRate.hs
+++ Biolab/Analysis/GrowthRate.hs
@@ -8,7 +8,7 @@ import qualified Data.Vector as V
 import Data.Vector ((!))
 import Statistics.Sample (Sample, mean)
 import Statistics.LinearRegression (nonRandomRobustFit, defaultEstimationParameters)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, isJust, fromJust)
 import Data.Time (UTCTime, NominalDiffTime, diffUTCTime)
 import Statistics.Function (sortBy)
 import Data.Function (on)
@@ -24,20 +24,22 @@ absoluteToRelativeTime v = (start, V.map (\(x,y) -> (x `diffUTCTime` start,y)) v
     where
         start = fst . V.head $ v
 
-minDoublingTime :: V.Vector (UTCTime,NormalizedMeasurement) -> NominalDiffTime
-minDoublingTime = realToFrac . mean . V.map (realToFrac . snd) . V.take 3 . V.drop 2 . sortBy (compare `on` snd) . doublingTime Nothing
+minDoublingTime :: V.Vector (UTCTime,NormalizedMeasurement) -> Maybe NominalDiffTime
+minDoublingTime = result . V.map (realToFrac . snd) . V.take 3 . V.drop 2 . sortBy (compare `on` snd) . V.map (\(x,y) -> (x,fromJust y)) . V.filter (isJust . snd) . doublingTime Nothing
+    where result v = if V.null v then Nothing else Just . realToFrac . mean $ v
 
-doublingTime :: Maybe Int -> V.Vector (UTCTime, NormalizedMeasurement) -> V.Vector (NominalDiffTime,NominalDiffTime)
+doublingTime :: Maybe Int -> V.Vector (UTCTime, NormalizedMeasurement) -> V.Vector (NominalDiffTime,Maybe NominalDiffTime)
 doublingTime mws mes = V.fromList . map (doublingTimeWindow window_size . V.take window_size) . takeWhile (\x -> V.length x >= window_size) . iterate V.tail . V.map (realToFrac *** (logBase 2 . nmVal)) $ dmes
     where
         (start,dmes) = absoluteToRelativeTime mes
         window_size = fromMaybe windowSize mws
 
-doublingTimeWindow :: Int -> V.Vector (Double,Double) -> (NominalDiffTime,NominalDiffTime)
+doublingTimeWindow :: Int -> V.Vector (Double,Double) -> (NominalDiffTime,Maybe NominalDiffTime)
 doublingTimeWindow window_size v = (realToFrac . fst $ v ! (window_size `div` 2),calcDoublingTime v)
 
-calcDoublingTime :: V.Vector (Double,Double) -> NominalDiffTime
-calcDoublingTime xys = realToFrac . (1/) . snd . nonRandomRobustFit defaultEstimationParameters xs $ ys
+calcDoublingTime :: V.Vector (Double,Double) -> Maybe NominalDiffTime
+calcDoublingTime xys = fmap (realToFrac . (1/)) . gr . snd . nonRandomRobustFit defaultEstimationParameters xs $ ys
     where
         (xs,ys) = U.unzip . U.fromList . V.toList $ xys
+        gr x = if x <=0 then Nothing else Just x
 
